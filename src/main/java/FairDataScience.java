48ccbc75726102daf597309fd1566232d21b1a83
Michael Sober <michael.sober@ymail.com>
2018-03-21 16:33:24 +0100

    Added crimes of germany

--- src/main/java/FairDataScience.java
+++ src/main/java/FairDataScience.java
@@ -3,11 +3,15 @@ import com.sun.prism.impl.Disposer.Record;
 import java.io.FileReader;
 import java.io.Reader;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -29,65 +33,100 @@ public class FairDataScience extends Application {
         primaryStage.setTitle("Hello World");
 
 
-        String[] HEADERS = { "ID","Case Number","Date","Block","IUCR","Primary Type","Description"
+        String[] CHICAGO_CRIME_HEADERS = { "ID","Case Number","Date","Block","IUCR","Primary Type","Description"
             ,"Location Description","Arrest","Domestic","Beat","District","Ward","Community Area"
             ,"FBI Code","X Coordinate","Y Coordinate","Year","Updated On","Latitude","Longitude"
             ,"Location"
         };
 
-        Reader in = new FileReader(getClass().getResource("Crimes_-_2001_to_present.csv").getPath());
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT
-            .withHeader(HEADERS)
+        String[] GERMANY_CRIME_HEADERS = {"Art","1976","1977","1978","1979","1980","1981","1982","1983","1984"
+            , "1985","1986","1987","1988","1989", "1990","1991","1992","1993","1994"
+            ,"1995","1996","1997", "1998","1999","2000","2001","2002","2003","2004","2005"
+            ,"2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016"
+        };
+
+
+        Reader chicagoFileReader = new FileReader(getClass().getResource("Crimes_-_2001_to_present.csv").getPath());
+        Iterable<CSVRecord> chicagoRecords = CSVFormat.DEFAULT
+            .withHeader(CHICAGO_CRIME_HEADERS)
             .withSkipHeaderRecord()
-            .parse(in);
+            .parse(chicagoFileReader);
 
-        Map<Integer, Long> crimesPerYear = new TreeMap<>();
+        Reader germanyFileReader = new FileReader(getClass().getResource("Crimes_-_1976_to_2016.csv").getPath());
+        Iterable<CSVRecord> germanyRecords = CSVFormat.DEFAULT
+            .withHeader(GERMANY_CRIME_HEADERS)
+            .withSkipHeaderRecord()
+            .parse(germanyFileReader);
 
-        for (CSVRecord record : records) {
+        Map<Integer, Long> crimesPerYearChicago = new TreeMap<>();
+        Map<Integer, Long> crimesPerYearGermany = new TreeMap<>();
+
+
+        for (CSVRecord record : chicagoRecords) {
 
             String date = record.get("Date");
             String year = date.substring(date.lastIndexOf("/")+1, date.lastIndexOf("/")+5);
-            //System.out.println(year);
             Integer yearVal = Integer.valueOf(year);
 
-            if(!yearVal.equals(2018)){
+            if(!yearVal.equals(2018) && !yearVal.equals(2017)){
 
-                if(!crimesPerYear.containsKey(yearVal)){
-                    crimesPerYear.put(yearVal, 0L);
+                if(!crimesPerYearChicago.containsKey(yearVal)){
+                    crimesPerYearChicago.put(yearVal, 0L);
                 }else{
-                    crimesPerYear.put(yearVal, crimesPerYear.get(yearVal)+1);
+                    crimesPerYearChicago.put(yearVal, crimesPerYearChicago.get(yearVal)+1);
                 }
 
             }
 
         }
 
+
+        Iterator<CSVRecord> csvRecordIterator = germanyRecords.iterator();
+        CSVRecord record = csvRecordIterator.next();
+        Map<String, String> map = record.toMap();
+        Collection<String> values = map.values();
+        List<String> someList = Lists.newArrayList(values);
+        String[] valueArray = someList.get(0).split(";");
+
+        for(int i = 26; i < GERMANY_CRIME_HEADERS.length; i++){
+            //System.out.println(record.get("1976"));
+
+            String year = GERMANY_CRIME_HEADERS[i];
+            System.out.println(year + " " + valueArray[i]);
+                crimesPerYearGermany.put(Integer.valueOf(year), Long.valueOf(valueArray[i]));
+
+
+        }
+
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Year");
         yAxis.setLabel("Number of crimes");
-        xAxis.setLabel("Month");
+
         final LineChart<String,Number> lineChart = new LineChart<>(xAxis,yAxis);
+        lineChart.setTitle("Incidents of crime in the City of Chicago correlates with incidents of crime in Germany");
 
-        lineChart.setTitle("Incidents of crime in the City of Chicago correlates with <Example>");
+        XYChart.Series chicagoSeries = new XYChart.Series();
+        chicagoSeries.setName("Crimes in Chicago");
 
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Crimes in Chicago");
+        for(Map.Entry<Integer, Long> entry : crimesPerYearChicago.entrySet()){
+            chicagoSeries.getData().add(new XYChart.Data(entry.getKey() + "", entry.getValue()));
+        }
 
-        for(Map.Entry<Integer, Long> entry : crimesPerYear.entrySet()){
-            series1.getData().add(new XYChart.Data(entry.getKey() + "", entry.getValue()));
+        XYChart.Series germanySeries = new XYChart.Series();
+        germanySeries.setName("Crimes in Germany");
+
+        for(Map.Entry<Integer, Long> entry : crimesPerYearGermany.entrySet()){
+            germanySeries.getData().add(new XYChart.Data(entry.getKey() + "", entry.getValue()));
         }
 
         Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().addAll(series1);
-
-
+        lineChart.getData().addAll(chicagoSeries, germanySeries);
 
         primaryStage.setScene(scene);
-        //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
